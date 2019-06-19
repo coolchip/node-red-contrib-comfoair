48f69d48eff9a405b2e3fe87972fb47f19913c62
Sebastian Balling <coolchip@gmx.de>
2019-06-19 21:38:53 +0200

    improove error logging

--- comfoair.js
+++ comfoair.js
@@ -15,10 +15,6 @@ module.exports = function (RED) {
             node.comfoair = comfoairPool.get(this.comfoairDatasource.serialport,
                 this.comfoairDatasource.serialbaud);
 
-            node.comfoair.on('error', function (err) {
-                return RED.log.error(`comfoair error: ${err}`);
-            });
-
             node.comfoair.on('ready', function () {
                 node.status({
                     fill: 'green',
@@ -43,7 +39,7 @@ module.exports = function (RED) {
 
                     node.comfoair.runCommand(msg.payload.name, msg.payload.params, (err, resp) => {
                         if (err) {
-                            const errMsg = err.toString().replace('Serialport', 'Serialport ' + node.comfoair.port);
+                            const errMsg = `comfoair [${node.comfoair.port}] run command '${msg.payload.name}': ${err.message}`;
                             return node.error(errMsg, msg);
                         }
                         if (resp) {
@@ -70,92 +66,94 @@ module.exports = function (RED) {
     const comfoairPool = (function () {
         const connections = {};
         return {
-            get: function (port, baud) {
+            get(port, baud) {
+                // just return the connection object if already have one
+                // key is the port (file path)
                 const id = port;
-                if (!connections[id]) {
-                    connections[id] = (function () {
-                        const obj = {
-                            _emitter: new events.EventEmitter(),
-                            comfoair: null,
-                            _closing: false,
-                            tout: null,
-                            port: port,
-                            on: function (eventName, cb) {
-                                this._emitter.on(eventName, cb);
+                if (connections[id]) return connections[id];
+                
+                connections[id] = (function () {
+                    const obj = {
+                        _emitter: new events.EventEmitter(),
+                        comfoair: null,
+                        _closing: false,
+                        tout: null,
+                        port,
+                        on(eventName, cb) {
+                            this._emitter.on(eventName, cb);
+                        },
+                        close(cb) {
+                            this.comfoair.close(cb);
+                        },
+                        runCommand(name, params, cb) {
+                            this.comfoair.runCommand(name, params, cb);
+                        },
+                        isValidFunction(name) {
+                            return (typeof this.comfoair[name] === 'function');
+                        }
+                    };
+                    let olderr = '';
+                    const setupComfoair = function () {
+                        obj.comfoair = new Comfoair({
+                                port,
+                                baud
                             },
-                            close: function (cb) {
-                                this.comfoair.close(cb);
-                            },
-                            runCommand: function (name, params, cb) {
-                                this.comfoair.runCommand(name, params, cb);
-                            },
-                            isValidFunction: function (name) {
-                                return (typeof this.comfoair[name] === 'function');
-                            }
-                        };
-                        let olderr = '';
-                        const setupComfoair = function () {
-                            obj.comfoair = new Comfoair({
-                                    port: port,
-                                    baud: baud
-                                },
-                                function (err) {
-                                    if (err) {
-                                        if (err.toString() !== olderr) {
-                                            olderr = err.toString();
-                                            RED.log.error(RED._('comfoair.errors.error', {
-                                                port: port,
-                                                error: olderr
-                                            }));
-                                        }
-                                        obj.tout = setTimeout(function () {
-                                            setupComfoair();
-                                        }, settings.serialReconnectTime);
+                            function (err) {
+                                if (err) {
+                                    if (err.toString() !== olderr) {
+                                        olderr = err.toString();
+                                        RED.log.error(RED._('comfoair.errors.error', {
+                                            port,
+                                            error: olderr
+                                        }));
                                     }
-                                });
-                            obj.comfoair.on('error', function (err) {
-                                RED.log.error(RED._('comfoair.errors.error', {
-                                    port: port,
-                                    error: err.toString()
-                                }));
-                                obj._emitter.emit('closed');
-                                obj.tout = setTimeout(function () {
-                                    setupComfoair();
-                                }, settings.serialReconnectTime);
-                            });
-                            obj.comfoair.on('close', function () {
-                                if (!obj._closing) {
-                                    RED.log.error(RED._('comfoair.errors.unexpected-close', {
-                                        port: port
-                                    }));
-                                    obj._emitter.emit('closed');
                                     obj.tout = setTimeout(function () {
                                         setupComfoair();
                                     }, settings.serialReconnectTime);
                                 }
                             });
-                            obj.comfoair.on('open', function () {
-                                olderr = '';
-                                RED.log.info(RED._('comfoair.onopen', {
-                                    port: port,
-                                    baud: baud
+                        obj.comfoair.on('error', function (err) {
+                            RED.log.error(RED._('comfoair.errors.error', {
+                                port,
+                                error: err.toString()
+                            }));
+                            obj._emitter.emit('closed');
+                            obj.tout = setTimeout(function () {
+                                setupComfoair();
+                            }, settings.serialReconnectTime);
+                        });
+                        obj.comfoair.on('close', function () {
+                            if (!obj._closing) {
+                                RED.log.error(RED._('comfoair.errors.unexpected-close', {
+                                    port
                                 }));
-                                if (obj.tout) {
-                                    clearTimeout(obj.tout);
-                                }
-                                obj._emitter.emit('ready');
-                            });
-                            obj.comfoair.on('data', function (d) {
-                                obj._emitter.emit('data', d);
-                            });
-                        };
-                        setupComfoair();
-                        return obj;
-                    }());
-                }
+                                obj._emitter.emit('closed');
+                                obj.tout = setTimeout(function () {
+                                    setupComfoair();
+                                }, settings.serialReconnectTime);
+                            }
+                        });
+                        obj.comfoair.on('open', function () {
+                            olderr = '';
+                            RED.log.info(RED._('comfoair.onopen', {
+                                port,
+                                baud
+                            }));
+                            if (obj.tout) {
+                                clearTimeout(obj.tout);
+                            }
+                            obj._emitter.emit('ready');
+                        });
+                        obj.comfoair.on('data', function (d) {
+                            obj._emitter.emit('data', d);
+                        });
+                    };
+                    setupComfoair();
+                    return obj;
+                }());
                 return connections[id];
             },
-            close: function (port, done) {
+            close(port, done) {
                 if (connections[port]) {
                     if (connections[port].tout != null) {
                         clearTimeout(connections[port].tout);
@@ -164,7 +162,7 @@ module.exports = function (RED) {
                     try {
                         connections[port].close(function () {
                             RED.log.info(RED._('comfoair.errors.closed', {
-                                port: port
+                                port
                             }));
                             done();
                         });
